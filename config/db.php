406c5ff8775d0189734c9f88c6995966a4eca4b6
Belowe1 <161724149+Belowe1@users.noreply.github.com>
2026-04-23 11:39:26 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = '127.0.0.1';
 $dbname = 'clothing_shop';
-$username = 'shopuser';
-$password = 'shop123';
+$username = 'root';
+$password = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
